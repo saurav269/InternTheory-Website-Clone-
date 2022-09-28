b5d13d2b7d3c7884f0ecb22f0d32b813dfa0f0b7
saurav269 <saurav786@gmail.com>
2022-09-29 02:56:40 +0530

    renew

--- signup.js
+++ signup.js
@@ -6,5 +6,28 @@ function details(n,ln,em,p){
     this.lastname = ln;
     this.email = em;
     this.password = p
+}
+let arr = JSON.parse(localStorage.getItem("user")) || []
+
+document.getElementById("next").addEventListener("click",function(){
+    myfun()
+})
+
+const myfun = () => {
+
+    let name = document.getElementById("name").value;
+    let lastName = document.getElementById("lName").value;
+    let email = document.getElementById("mail").value;
+    let password = document.getElementById("password").value;
+
+    let s1 = new details(name,lastName,email,password)
+
+    if(s1.name == "" || s1.lastName == "" || s1.email == "" || s1.password == ""){
+        alert("Some Fields are missing")
+    }else{
+        arr.push(s1)
+        localStorage.setItem("user",JSON.stringify(arr))
+        alert("Registration Successful!")
+    }
 
 }
