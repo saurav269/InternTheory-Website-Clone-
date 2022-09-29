d22cfb885c71a44b00c7738575ab668ef8185234
satya M <satya2maity@gmail.com>
2022-09-29 21:51:01 +0530

    added new

---
+++ sidebar.js
@@ -0,0 +1,16 @@
+// num=1
+//     num1=1
+//     const color=()=>{
+//         let color=document.getElementById("color_name")
+//         // console.log("working")
+//         if(num%2!=0){
+//             color.style.display="block"
+//             num++
+//         }
+//         else{
+//             color.style.display="none"
+//             num++
+//         }
+     
+        
+//     }
